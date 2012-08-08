4114c2249c4ad099a7d0c6a07506e763a760321a
Jon Phenow <j.phenow@gmail.com>
2012-08-07 19:49:03 -0500

    remove things we shouldn't worry about within this gem

--- lib/ical_importer/recurring_builder.rb
+++ lib/ical_importer/recurring_builder.rb
@@ -15,8 +15,6 @@ module IcalImporter
       # raise "iCal feed (#{id}) does not contain UID" if remote_event.uid.blank?
       @events_to_build.each do |remote_event|
         if recurring_event = Event.find_by_ical_uid(remote_event.uid)
-          DateExclusion.create(:exclude_date => remote_event.recurrence_id, :event_id => recurring_event.id)
-          safe_save recurring_event
           @built_events << build_new_local_event(remote_event)
         end
       end
@@ -26,30 +24,16 @@ module IcalImporter
     private
 
     def build_new_local_event(remote_event)
-      local_event = Event.new
-      local_event.attributes = {
-        :ical_feed => self,
+      local_event = EventScaffold.new({
+        #:ical_feed => self, # TODO
         :title => remote_event.summary,
         :description => local_event.description,
         :location => remote_event.location || '',
         :start_date_time => local_event.start_date_time,
-        :end_date_time => local_event.end_date_time
-      }
-      local_event.page_nodes = self.page_nodes # overwrite event's tags # TODO GET ME SOME IDS
-      safe_save local_event
-    end
-
-    def safe_save(object)
-      begin
-        object.save!
-      rescue ActiveRecord::RecordNotUnique => e
-        Ngin.log :ical_feed,
-          :object => recurring_event,
-          :error => object,
-          :message => "Duplicate key error",
-          :custom_fields => object.attributes
-      end
-      object unless object.new_record?
+        :end_date_time => local_event.end_date_time,
+        :date_exclusions => [DateExclusion.new(:exclude_date => remote_event.recurrence_id)]
+      })
+      #local_event.page_nodes = self.page_nodes # overwrite event's tags # TODO GET ME SOME IDS
     end
   end
 end
