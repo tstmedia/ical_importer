bbcfc4adf969ee817ac538aaafec2d623c7da556
Andy Fleener <andrew.fleener@tstmedia.com>
2012-08-29 09:57:37 -0500

    Fixed bug with date exclusions

--- spec/ical_importer/date_exclusion_spec.rb
+++ spec/ical_importer/date_exclusion_spec.rb
@@ -1,28 +1,20 @@
 require 'spec_helper'
 module IcalImporter
   describe DateExclusion do
-    subject { DateExclusion.new(attributes) }
+    subject { DateExclusion.new(date) }
     describe "#initialize" do
       describe "set the one valid attribute" do
-        let(:attributes) { { :date_exclusion => "yerp" } }
+        let(:date) {  "yerp" }
         it "sets the date exlcusion" do
           subject.date_exclusion.should == "yerp"
         end
       end
 
       describe "try to set valid/invalid attributes" do
-        let(:attributes) { { :date_exclusion => "yerp", :not_me => "nope" } }
+        let(:date) { "yerp" }
         it "sets the date exlcusion" do
           subject.date_exclusion.should == "yerp"
           expect { subject.not_me }.to raise_error(NoMethodError)
-        end
-      end
-
-      describe "set the one valid attribute" do
-        let(:attributes) { { :not_me => "yerp", :or_me => "nope" } }
-        it "sets the date exlcusion" do
-          expect { subject.not_me }.to raise_error(NoMethodError)
-          expect { subject.or_me }.to raise_error(NoMethodError)
         end
       end
     end
